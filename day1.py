c650139d2e758b3a15024a2113a0b01aab2dd0d6
Sebastian Feliciano <felicianosebastiane@gmail.com>
2026-06-14 17:08:09 -0400

    commit day1

---
+++ day1.py
@@ -0,0 +1,8 @@
+# Defining the variable to assemble the project
+print("Welcome to the Band Name Generator!")
+
+city_name = input("what's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+
+# Giving the name of the band
+print("Your band name could be " + city_name + " " + pet_name)
